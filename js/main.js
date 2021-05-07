9c884009a69151424c495efc290a657ef5267434
Paulo Ramon Nogueira de Freitas <pramon71@gmail.com>
2021-05-07 11:07:45 -0300

    Estudo de Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
+/* Array de Dicionário */
+frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+console.log(frutas[0]);
+console.log(frutas[0].nome);
+console.log(frutas[0].cor);
+
+/* Operações com Dicionário 
+fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/* Operações com Arrays
+var lista = ["Maçã", "Pêra", "Laranja"];
+lista.push("Uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+/* Operação com Variáveis - Inteiro e String
 var nome = "João Gilberto";
 var n1 = 27;
 var n2 = 10;
 var frase = "O Tosco é o pior presidente do mundo."
-
+*/
 /* Chamada da função alert() */
 //alert(nome + " tem " + n1 + " anos.");
-
+/*
 console.log(nome);
 console.log(n1 + n2);
 console.log(frase.replace("Tosco", "Satan Goss"));
 console.log(frase.toLocaleUpperCase());
 console.log(frase.toLocaleLowerCase());
+*/
