d53ed276e64ab63d994f4465f3f326bdc6b7b344
Paulo Ramon Nogueira de Freitas <pramon71@gmail.com>
2021-05-06 21:21:59 -0300

    Estudo de Inteiros e Variáveis

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "João Gilberto";
+var n1 = 27;
+var n2 = 10;
+var frase = "O Tosco é o pior presidente do mundo."
+
+/* Chamada da função alert() */
+//alert(nome + " tem " + n1 + " anos.");
+
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.replace("Tosco", "Satan Goss"));
+console.log(frase.toLocaleUpperCase());
+console.log(frase.toLocaleLowerCase());
